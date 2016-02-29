789424678fd80c6a32d0533966335ec24541dfad
Lynsey Teague <lynsey.teague@yahoo.com>
2016-02-29 18:54:39 -0500

    experimenting with forms

--- js/myApp.js
+++ js/myApp.js
@@ -1,9 +1,25 @@
+var myNewModule = angular.module('myNewModule', [
+    'ngMessages'
+]);
+
 angular.module('myApp', [])
-    .controller('myCtrl', function($scope) {
+    .controller('myCtrl', function ($scope) {
         $scope.countries = [
-            {name: "Burundi" , code:"bi"},
-            {name: "Belize", code:"bz"},
-            {name: "Egypt", code:"eg"},
-            {name: "Philippines", code:"ph"}
+            {
+                name: "Burundi",
+                code: "bi"
+            },
+            {
+                name: "Belize",
+                code: "bz"
+            },
+            {
+                name: "Egypt",
+                code: "eg"
+            },
+            {
+                name: "Philippines",
+                code: "ph"
+            }
         ];
     });
